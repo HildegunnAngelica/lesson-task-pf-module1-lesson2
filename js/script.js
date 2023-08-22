39c5daca91ef2af65e5cbc170a4e9123df8d6074
HildegunnAngelica <Hildegunnangelicas@gmail.com>
2023-08-22 20:47:33 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,81 @@
+//question 1
+
+var integer = 15;
+console.log(integer);
+
+var decimal = 15.5;
+console.log(decimal);
+
+
+//question 2
+
+var workDays = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+console.log(workDays);
+
+//question 3 
+
+var addition = 2 + 2;
+console.log(addition);
+
+var subtraction = 4 - 2;
+console.log(subtraction);
+
+var multiplication = 4 * 10;
+console.log(multiplication);
+
+var division = 40 / 2;
+console.log(division);
+
+//question 4 
+
+var initialTrue = true;
+console.log(initialTrue);
+
+var initialFalse = false;
+console.log(initialFalse);
+
+//question 5 
+
+var numberOfCharacters = "The frogs are angry";
+console.log(numberOfCharacters.length);
+
+//question 6
+
+var timeElapsed
+var totalPrice
+
+//question 7 
+
+var number1 = 53;
+console.log(number1);
+
+var number2 = 44.6;
+console.log(number2);
+
+//question 8 
+
+var pet = "donkey";
+console.log(pet);
+
+var age = 89;
+console.log(age);
+
+var discountApplied = true;
+console.log(discountApplied);
+ 
+var timeLeft;
+console.log(timeLeft);
+
+//question 9 
+
+var headingText = "Updating heading";
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+//question 10 
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
+
+
+
